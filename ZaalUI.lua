775ee32314ad78c805108d5d7b9cf0f59f5a851c
davidmottet <mottet.tlse@gmail.com>
2018-08-06 23:08:23 +0200

    update

--- ZaalUI.lua
+++ ZaalUI.lua
@@ -1,84 +1,40 @@
---Hides stuff
---PlayerFrame:Hide();
---MicroButtonAndBagsBar:Hide() 
---StatusTrackingBarManager:Hide();
---MainMenuBarArtFrame.RightEndCap:Hide();
---MainMenuBarArtFrame.LeftEndCap:Hide();
---UIErrorsFrame:Hide() 
---StanceButton1:SetAlpha(0)
---QuickJoinToastButton:Hide()
---ChatFrameChannelButton:Hide()
+local function CommunMode()
+	--Hides stuff
+	MicroButtonAndBagsBar:Hide() 
 
---Removes gap
---MainMenuBarArtFrameBackground:ClearAllPoints() 
---MainMenuBarArtFrameBackground:SetPoint("CENTER",0,-23) MainMenuBarArtFrameBackground.SetPoint = function() end
+	--Player/target/focus adjust
+	TargetFrame:ClearAllPoints()
+	TargetFrame:SetPoint("TOPLEFT",GetScreenWidth()/5,-GetScreenHeight()/5) TargetFrame.SetPoint = function() end
 
-local function HealerMode(  )
-  --Player/target/focus adjust
-  Display personal
-  PlayerFrame:SetAlpha(0)
-
-  TargetFrame:ClearAllPoints()
-  TargetFrame:SetPoint("TOPLEFT",GetScreenWidth()/4,-GetScreenHeight()/4)
-
-  FocusFrame:ClearAllPoints() 
-  FocusFrame:SetPoint("TOPLEFT",GetScreenWidth()/4,-(GetScreenHeight()/4)*2)
+	FocusFrame:ClearAllPoints() 
+	FocusFrame:SetPoint("TOPLEFT",GetScreenWidth()/5,-(GetScreenHeight()/5)*2) FocusFrame.SetPoint = function() end
 end
 
-
-local function DpsMode(  )
-  --Player/target/focus adjust
-  Display personal
-  PlayerFrame:SetAlpha(0)
-
-  TargetFrame:ClearAllPoints()
-  TargetFrame:SetPoint("TOPLEFT",GetScreenWidth()/4,-GetScreenHeight()/4)
-
-  FocusFrame:ClearAllPoints() 
-  FocusFrame:SetPoint("TOPLEFT",GetScreenWidth()/4,-(GetScreenHeight()/4)*2)
+local function HealerMode()
 end
 
-
-local function NormalMode(  )
-  --Player/target/focus adjust
-  Display personal
-  PlayerFrame:SetAlpha(1)
+local function DpsMode()
+	PlayerFrame:SetAlpha(0)
 end
 
-local function ZaalUICommands( msg, editbox )
-  if ( msg == 'healer' ) then
-    HealerMode()
-  elseif ( msg == 'dps' ) then
-    DpsMode()
-  else
-    NormalMode()
-  end
+local function NormalMode()
+	PlayerFrame:SetAlpha(1)
 end
 
-<<<<<<< HEAD
-FocusFrame:ClearAllPoints() 
+local function ZaalCommands(msg, editbox)
+	CommunMode()
+	if ( msg == 'healer' ) then
+		HealerMode()
+	elseif ( msg == 'dps' ) then
+		DpsMode()
+	else
+		NormalMode()
+	end
+end
 
+SLASH_ZAAL1, SLASH_ZAAL2 = '/zl', '/zaal'
 
-FocusFrame:SetPoint("TOPLEFT",387,-530) FocusFrame.SetPoint = function() end
-
-
-
---Adds button to right of Bottombar
-
---MultiBarLeftButton12:ClearAllPoints()
-
---MultiBarLeftButton12:SetPoint("LEFT", MultiBarBottomLeftButton12, "RIGHT", 6, 0) MultiBarLeftButton12.SetPoint = function() end
-=======
-SLASH_ZALL1 = '/zaal'
->>>>>>> 4df9e351429d658713ecd799a10385ad1687d0b5
-
-SlashCmdList["ZAAL"] = ZaalUICommands   -- add /zall command list with argument healer or dps
+SlashCmdList["ZAAL"] = ZaalCommands   -- add /zl and /zaal to command list
 
 --Last on raid frames
 LoadAddOn("Blizzard_CompactRaidFrames") CRFSort_Group=function(t1, t2) if UnitIsUnit(t1,"player") then return false elseif UnitIsUnit(t2,"player") then return true else return t1 < t2 end end CompactRaidFrameContainer.flowSortFunc=CRFSort_Group
-
---Sets variables
---SetCVar("useUIScale", 1)
---SetCVar("UIScale", 0.1)
---SetCVar("alwaysShowActionBars", 0)
---SetCVar("showHonorAsExperience", 1)
