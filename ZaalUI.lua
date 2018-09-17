225b40ba6b039d3f4a981fb996afc0e44701049c
davidmottet <mottet.tlse@gmail.com>
2018-09-18 01:00:03 +0200

    WIP commun state

--- ZaalUI.lua
+++ ZaalUI.lua
@@ -1,28 +1,32 @@
 local function CommunMode()
 	--Hides stuff
-	MicroButtonAndBagsBar:Hide() 
-
+	MicroButtonAndBagsBar:Hide()
+	PlayerFrame:SetAlpha(0)
+end
+local function FramePosition()
 	--Player/target/focus adjust
 	TargetFrame:ClearAllPoints()
-	TargetFrame:SetPoint("TOPLEFT",GetScreenWidth()/5,-GetScreenHeight()/5) TargetFrame.SetPoint = function() end
+	TargetFrame:SetPoint("TOPLEFT",GetScreenWidth()/5,-GetScreenHeight()/5)
+	TargetFrame.SetPoint = function() end
+	TargetFrame:SetUserPlaced(true)
 
 	FocusFrame:ClearAllPoints() 
-	FocusFrame:SetPoint("TOPLEFT",GetScreenWidth()/5,-(GetScreenHeight()/5)*2) FocusFrame.SetPoint = function() end
+	FocusFrame:SetPoint("TOPLEFT",GetScreenWidth()/5,-(GetScreenHeight()/5)*2)
+	FocusFrame.SetPoint = function() end
+	FocusFrame:SetUserPlaced(true)
 end
 
 local function HealerMode()
 end
 
 local function DpsMode()
-	PlayerFrame:SetAlpha(0)
 end
 
 local function NormalMode()
-	PlayerFrame:SetAlpha(1)
 end
 
 local function ZaalCommands(msg, editbox)
-	CommunMode()
+	FramePosition()
 	if ( msg == 'healer' ) then
 		HealerMode()
 	elseif ( msg == 'dps' ) then
@@ -31,10 +35,11 @@ local function ZaalCommands(msg, editbox)
 		NormalMode()
 	end
 end
+CommunMode()
 
 SLASH_ZAAL1, SLASH_ZAAL2 = '/zl', '/zaal'
 
 SlashCmdList["ZAAL"] = ZaalCommands   -- add /zl and /zaal to command list
 
 --Last on raid frames
-LoadAddOn("Blizzard_CompactRaidFrames") CRFSort_Group=function(t1, t2) if UnitIsUnit(t1,"player") then return false elseif UnitIsUnit(t2,"player") then return true else return t1 < t2 end end CompactRaidFrameContainer.flowSortFunc=CRFSort_Group
+--LoadAddOn("Blizzard_CompactRaidFrames") CRFSort_Group=function(t1, t2) if UnitIsUnit(t1,"player") then return false elseif UnitIsUnit(t2,"player") then return true else return t1 < t2 end end CompactRaidFrameContainer.flowSortFunc=CRFSort_Group
